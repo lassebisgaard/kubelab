dfc78abff45a723d4cb72649e76f31355b6aeab9
Sarahtlenglish <sarah_english@live.dk>
2024-12-16 23:03:15 +0100

    Nye database

--- Kubelab.sql
+++ Kubelab.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Vært: mysql
--- Genereringstid: 10. 12 2024 kl. 14:16:49
--- Serverversion: 8.4.3
--- PHP-version: 8.2.26
+-- Genereringstid: 16. 12 2024 kl. 22:02:44
+-- Serverversion: 8.4.2
+-- PHP-version: 8.2.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,8 +42,9 @@ CREATE TABLE `Projects` (
 --
 
 INSERT INTO `Projects` (`ProjectId`, `ProjectName`, `Domain`, `Description`, `DateCreated`, `UserId`, `TemplateId`) VALUES
-(36, 'myawesomewp', 'myawesomewp', '', '2024-12-09 20:42:00', 1, 34),
-(37, 'mitnyeste', 'mitnyeste', '', '2024-12-09 20:45:44', 1, 33);
+(36, 'myawesomewp', 'myawesomewp', '', '2024-12-09 20:42:00', 14, 34),
+(37, 'mitnyeste', 'mitnyeste', '', '2024-12-09 20:45:44', 14, 33),
+(85, 'intetvirkeriwpland', 'intetvirkeriwpland', '', '2024-12-09 20:45:44', 11, 34);
 
 -- --------------------------------------------------------
 
@@ -62,8 +63,8 @@ CREATE TABLE `Roles` (
 --
 
 INSERT INTO `Roles` (`RoleId`, `IsAdmin`, `UserId`) VALUES
-(1, 0, 10),
-(2, 1, 11);
+(2, 1, 11),
+(5, 0, 14);
 
 -- --------------------------------------------------------
 
@@ -85,7 +86,8 @@ INSERT INTO `Services` (`ServiceId`, `ServiceName`, `Icon`) VALUES
 (1, 'WordPress', 'bxl-wordpress'),
 (2, 'MySQL', 'bx-data'),
 (3, 'phpMyAdmin', 'bx-server'),
-(4, 'Nginx', 'bx-server');
+(4, 'Nginx', 'bx-server'),
+(24, 'fefef', 'bx-cloud');
 
 -- --------------------------------------------------------
 
@@ -96,18 +98,17 @@ INSERT INTO `Services` (`ServiceId`, `ServiceName`, `Icon`) VALUES
 CREATE TABLE `Teams` (
   `TeamId` int NOT NULL,
   `Expiration` date NOT NULL,
-  `TeamName` varchar(255) NOT NULL
+  `TeamName` varchar(255) NOT NULL,
+  `Description` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Data dump for tabellen `Teams`
 --
 
-INSERT INTO `Teams` (`TeamId`, `Expiration`, `TeamName`) VALUES
-(9, '2024-12-19', 'WUOE24'),
-(11, '2024-12-26', 'Undervisere'),
-(12, '2025-04-17', 'sygeplejersker'),
-(13, '2025-03-12', 'almindeligemennesker');
+INSERT INTO `Teams` (`TeamId`, `Expiration`, `TeamName`, `Description`) VALUES
+(9, '2024-12-19', 'WUOE24', NULL),
+(11, '2024-12-26', 'Undervisere', NULL);
 
 -- --------------------------------------------------------
 
@@ -130,8 +131,8 @@ CREATE TABLE `Templates` (
 --
 
 INSERT INTO `Templates` (`TemplateId`, `TemplateName`, `Description`, `DateCreated`, `YamlContent`, `PreviewImage`, `UserId`) VALUES
-(33, 'A simple Nginx server', 'This is a simple nginxX webserver. That can be used by everyone, as a backend for their application. ', '2024-12-09 20:34:45', '{\n  \"networks\": {\n    \"traefik-proxy\": {\n      \"external\": true\n    }\n  },\n  \"services\": {\n    \"test\": {\n      \"image\": \"nginx:latest\",\n      \"networks\": [\"traefik-proxy\"],\n      \"deploy\": {\n        \"labels\": [\n          \"traefik.enable=true\",\n          \"traefik.http.routers.CHANGEME01.rule=Host(`SUBDOMAIN01.kubelab.dk`)\",\n          \"traefik.http.routers.CHANGEME.entrypoints=web,websecure\",\n          \"traefik.http.routers.CHANGEME.tls.certresolver=letsencrypt\",\n          \"traefik.http.services.CHANGEME.loadbalancer.server.port=80\"\n        ]\n      }\n    }\n  }\n}', '1733792637829.png', 1),
-(34, 'Full stack Wordpress', 'This is a a full stack wordpress konfiguration, including wp, mySQL and PHPmyAdmin', '2024-12-09 20:41:43', 'networks:\n  traefik-proxy:\n    external: true\n  wp-network:\n    driver: overlay\nservices:\n  wordpress:\n    image: wordpress:latest\n    environment:\n      WORDPRESS_DB_HOST: db\n      WORDPRESS_DB_USER: wpuser\n      WORDPRESS_DB_PASSWORD: wppassword\n      WORDPRESS_DB_NAME: wpdatabase\n    networks:\n      - traefik-proxy\n      - wp-network\n    deploy:\n      labels:\n        - traefik.enable=true\n        - traefik.http.routers.CHANGEME01.rule=Host(`SUBDOMAIN01.kubelab.dk`)\n        - traefik.http.routers.CHANGEME01.entrypoints=web,websecure\n        - traefik.http.routers.CHANGEME01.tls.certresolver=letsencrypt\n        - traefik.http.services.CHANGEME01.loadbalancer.server.port=80\n  db:\n    image: mariadb:latest\n    environment:\n      MYSQL_ROOT_PASSWORD: rootpassword\n      MYSQL_DATABASE: wpdatabase\n      MYSQL_USER: wpuser\n      MYSQL_PASSWORD: wppassword\n    networks:\n      - wp-network\n  phpmyadmin:\n    image: phpmyadmin:latest\n    environment:\n      PMA_HOST: db\n      PMA_USER: wpuser\n      PMA_PASSWORD: wppassword\n    networks:\n      - traefik-proxy\n      - wp-network\n    deploy:\n      labels:\n        - traefik.enable=true\n        - traefik.http.routers.CHANGEME02.rule=Host(`SUBDOMAIN02.kubelab.dk`)\n        - traefik.http.routers.CHANGEME02.entrypoints=web,websecure\n        - traefik.http.routers.CHANGEME02.tls.certresolver=letsencrypt\n        - traefik.http.services.CHANGEME02.loadbalancer.server.port=80', '1733792684959.png', 1);
+(33, 'A simple Nginx server', 'This is a simple nginxX webserver. That can be used by everyone, as a backend for their application. ', '2024-12-09 20:34:45', '{\n  \"networks\": {\n    \"traefik-proxy\": {\n      \"external\": true\n    }\n  },\n  \"services\": {\n    \"test\": {\n      \"image\": \"nginx:latest\",\n      \"networks\": [\"traefik-proxy\"],\n      \"deploy\": {\n        \"labels\": [\n          \"traefik.enable=true\",\n          \"traefik.http.routers.CHANGEME01.rule=Host(`SUBDOMAIN01.kubelab.dk`)\",\n          \"traefik.http.routers.CHANGEME.entrypoints=web,websecure\",\n          \"traefik.http.routers.CHANGEME.tls.certresolver=letsencrypt\",\n          \"traefik.http.services.CHANGEME.loadbalancer.server.port=80\"\n        ]\n      }\n    }\n  }\n}', '1733792637829.png', 11),
+(34, 'Full stack Wordpress', 'This is a a full stack wordpress konfiguration, including wp, mySQL and PHPmyAdmin', '2024-12-09 20:41:43', 'networks:\n  traefik-proxy:\n    external: true\n  wp-network:\n    driver: overlay\nservices:\n  wordpress:\n    image: wordpress:latest\n    environment:\n      WORDPRESS_DB_HOST: db\n      WORDPRESS_DB_USER: wpuser\n      WORDPRESS_DB_PASSWORD: wppassword\n      WORDPRESS_DB_NAME: wpdatabase\n    networks:\n      - traefik-proxy\n      - wp-network\n    deploy:\n      labels:\n        - traefik.enable=true\n        - traefik.http.routers.CHANGEME01.rule=Host(`SUBDOMAIN01.kubelab.dk`)\n        - traefik.http.routers.CHANGEME01.entrypoints=web,websecure\n        - traefik.http.routers.CHANGEME01.tls.certresolver=letsencrypt\n        - traefik.http.services.CHANGEME01.loadbalancer.server.port=80\n  db:\n    image: mariadb:latest\n    environment:\n      MYSQL_ROOT_PASSWORD: rootpassword\n      MYSQL_DATABASE: wpdatabase\n      MYSQL_USER: wpuser\n      MYSQL_PASSWORD: wppassword\n    networks:\n      - wp-network\n  phpmyadmin:\n    image: phpmyadmin:latest\n    environment:\n      PMA_HOST: db\n      PMA_USER: wpuser\n      PMA_PASSWORD: wppassword\n    networks:\n      - traefik-proxy\n      - wp-network\n    deploy:\n      labels:\n        - traefik.enable=true\n        - traefik.http.routers.CHANGEME02.rule=Host(`SUBDOMAIN02.kubelab.dk`)\n        - traefik.http.routers.CHANGEME02.entrypoints=web,websecure\n        - traefik.http.routers.CHANGEME02.tls.certresolver=letsencrypt\n        - traefik.http.services.CHANGEME02.loadbalancer.server.port=80', '1733792684959.png', 11);
 
 -- --------------------------------------------------------
 
@@ -164,21 +165,19 @@ CREATE TABLE `Users` (
   `UserId` int NOT NULL,
   `Name` varchar(255) NOT NULL,
   `Mail` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
+  `Password` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `Expiration` date NOT NULL,
-  `TeamId` int DEFAULT NULL
+  `TeamId` int DEFAULT NULL,
+  `AvatarSeed` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Data dump for tabellen `Users`
 --
 
-INSERT INTO `Users` (`UserId`, `Name`, `Mail`, `Password`, `Expiration`, `TeamId`) VALUES
-(1, 'sarah English', 'sarah_english@live.dk', 'minkode', '2024-12-19', 9),
-(2, 'Portainer James', 'james@live.dk', '1234', '2025-12-01', 9),
-(4, 'Søren Larsen', 'semlarsen@live.dk', 'khg4d1do', '2024-12-25', 11),
-(10, 'Lasse Biasgaard', 'lasseb@mail.com', 'hxnf07dn', '2025-04-30', 9),
-(11, 'Søren myman', 'sørenmyman@live.dk', '8tq2rar9', '2024-12-18', 13);
+INSERT INTO `Users` (`UserId`, `Name`, `Mail`, `Password`, `Expiration`, `TeamId`, `AvatarSeed`) VALUES
+(11, 'Admin', 'admin@live.dk', 'admin', '2024-12-18', 11, 'happy3'),
+(14, 'User', 'user@live.dk', 'user', '2025-01-30', 9, 'cute1');
 
 --
 -- Begrænsninger for dumpede tabeller
@@ -242,37 +241,37 @@ ALTER TABLE `Users`
 -- Tilføj AUTO_INCREMENT i tabel `Projects`
 --
 ALTER TABLE `Projects`
-  MODIFY `ProjectId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `ProjectId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
 
 --
 -- Tilføj AUTO_INCREMENT i tabel `Roles`
 --
 ALTER TABLE `Roles`
-  MODIFY `RoleId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `RoleId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Tilføj AUTO_INCREMENT i tabel `Services`
 --
 ALTER TABLE `Services`
-  MODIFY `ServiceId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `ServiceId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Tilføj AUTO_INCREMENT i tabel `Teams`
 --
 ALTER TABLE `Teams`
-  MODIFY `TeamId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `TeamId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Tilføj AUTO_INCREMENT i tabel `Templates`
 --
 ALTER TABLE `Templates`
-  MODIFY `TemplateId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `TemplateId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- Tilføj AUTO_INCREMENT i tabel `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `UserId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `UserId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Begrænsninger for dumpede tabeller
